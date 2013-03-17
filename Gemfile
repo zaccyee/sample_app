59889442d2d1f5cb39dbdd3654b0fc618bdc782d
Zacc Yee <zaccyee@gmail.com>
2013-03-17 19:03:26 +0800

    Finish Chapter 5

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0.beta1'
+gem 'bootstrap-sass'
 
 gem 'sqlite3'
 gem 'rspec-rails'
--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -12,32 +12,31 @@ require 'spec_helper'
 
 describe "Static pages" do
   describe "Home page" do
+    before { visit root_path }
 #    it "should have the content 'Sample App'" do 
 #      visit '/static_pages/home'
 #      page.should have_content('Sample App')
 #    end 
      it "should have the h1 'Sample App'" do
-       visit '/static_pages/home'
-       page.should have_selector('h1', :text => 'Sample App')
+       page.should have_selector('h1', text: 'Sample App')
      end
 
      it "should have the title 'Home'" do
-	visit '/static_pages/home'
-	page.should have_selector('title',
-				  :text => 'Sample App - Home')
+	     page.should have_selector('title',
+				  text: 'Sample App')
      end
   end
 
   describe "Help page" do
+    before { visit help_path }
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
       page.should have_content('Help')
     end
   end
 
   describe "About page" do
+    before { visit about_path }
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
       page.should have_content('About Us')
     end
   end
